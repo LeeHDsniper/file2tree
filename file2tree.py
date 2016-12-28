3deefc4456214f8446383c0bc4a9dc461f476a98
leehdsniper <leehdsniper@gmail.com>
2016-12-28 23:46:05 +0800

    some decode&encode problem

--- file2tree.py
+++ file2tree.py
@@ -1,5 +1,4 @@
-# -*- coding:utf-8 -*-
-
+# -*-coding:utf-8-*-
 import os
 import os.path
 import sys
@@ -12,63 +11,64 @@ def usage():
 
 
 def get_tree(cur_path):
-	stack=[]
-	ret=[]
-	stack.append(cur_path)
-	while len(stack)>0:
-		tmp=stack.pop(len(stack)-1)
-		if os.path.isdir(tmp):
-			ret.append(tmp)
-			for item in os.listdir(tmp):
-				stack.append(os.path.join(tmp,item))
-			#print tmp
-		elif os.path.isfile(tmp):
-			ret.append(tmp)
-			#print tmp
-	return ret
+    stack = []
+    ret = []
+    stack.append(cur_path)
+    while len(stack) > 0:
+        tmp = stack.pop(len(stack) - 1)
+        if os.path.isdir(tmp):
+            ret.append(tmp)
+            for item in os.listdir(tmp):
+                stack.append(os.path.join(tmp, item))
+            # print tmp
+        elif os.path.isfile(tmp):
+            ret.append(tmp)
+            # print tmp
+    return ret
+
 
 def write_tree(rootdir):
-	file_tree=open(os.path.join(rootdir,"/file2tree.txt"),"w")
-	path_list=get_tree(rootdir)
-	length=len(path_list)
-	#file_tree.write("©À©¤"+os.path.split(rootdir)[1]+"\n")
-	for index in range(1,length):
-		tmp=os.path.normcase(path_list[index].split(rootdir)[1])
-		tmp=tmp.strip("\\")
-		deepth=tmp.count("\\")+1
-		path_name=os.path.split(tmp)[1]
-		if(index<length-1):
-			tmp_next=os.path.normcase(path_list[index+1].split(rootdir)[1])
-			tmp_next=tmp_next.strip("\\")
-			deepth_next=tmp_next.count("\\")+1
-		else:
-			deepth_next=-1#©¦
-		if deepth_next==deepth or deepth_next>deepth:
-			space=""
-			for sp in range(0,deepth-1):
-				space+="©¦  "
-			file_tree.write(space+"©À©¤"+path_name+"\n")
-	
-		elif deepth_next!=deepth :
-			space=""
-			for sp in range(0,deepth-1):
-				space+="©¦  "
-			file_tree.write(space+"©¸©¤"+path_name+"\n")
-		#print os.path.split(i)[1]
-	file_tree.close()
-	
-	
+    file_tree = open(os.path.join(rootdir, "/file2tree.txt"), "w")
+    path_list = get_tree(rootdir)
+    length = len(path_list)
+    # file_tree.write("â”œâ”€"+os.path.split(rootdir)[1]+"\n")
+    for index in range(1, length):
+        tmp = os.path.normcase(path_list[index].split(rootdir)[1])
+        tmp = tmp.strip("\\")
+        deepth = tmp.count("\\") + 1
+        path_name = os.path.split(tmp)[1]
+        if(index < length - 1):
+            tmp_next = os.path.normcase(path_list[index + 1].split(rootdir)[1])
+            tmp_next = tmp_next.strip("\\")
+            deepth_next = tmp_next.count("\\") + 1
+        else:
+            deepth_next = -1
+        if deepth_next == deepth or deepth_next > deepth:
+            space = ""
+            for sp in range(0, deepth - 1):
+                space += "|  "
+            file_tree.write(space + "â”œâ”€" + path_name + "\n")
+
+        elif deepth_next != deepth:
+            space = ""
+            for sp in range(0, deepth - 1):
+                space += "|  "
+            file_tree.write(space + "â””â”€" + path_name + "\n")
+        # print os.path.split(i)[1]
+    file_tree.close()
+
+
 opts, args = getopt.getopt(sys.argv[1:], "hr:", ["help", "rootdir="])
 if not opts:
-	usage()
+    usage()
 for op, value in opts:
-	if op == "-r" or op == "--rootdir":
-		rootdir = value
-		if not os.path.exists(rootdir):
-			print '[ * ] path "%s" does not exist,please check the param "-r"' % (rootdir)
-			sys.exit()
-		else:
-			write_tree(rootdir.rstrip("\\"))	
-	else:
-		usage()
-		sys.exit()
+    if op == "-r" or op == "--rootdir":
+        rootdir = value
+        if not os.path.exists(rootdir):
+            print '[ * ] path "%s" does not exist,please check the param "-r"' % (rootdir)
+            sys.exit()
+        else:
+            write_tree(rootdir.rstrip("\\"))
+    else:
+        usage()
+        sys.exit()
