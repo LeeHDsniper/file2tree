28e0e3917891c7cd5d737a0ee0432dcdcace1781
leehdsniper <leehdsniper@gmail.com>
2016-12-29 14:11:55 +0800

    update

--- Readme.md
+++ Readme.md
@@ -1,6 +1,10 @@
-##file2tree
-###功能
+#file2tree
+##功能
 对一个文件夹进行遍历，生成如下图的文件树：
+
 ![Alt text](./screenshot.png)
 
-
+##注意
+- 在linux命令行下cat 生成的文件时，可能存在编码导致文件树乱码问题
+- 在用sublime编辑代码时，可能导致编码问题，最终导致生成的文件树文件存在中文和符号乱码
+- 建议使用notepad++或者系统自带的notepad、leafpad等打开、编辑代码和文件树
--- file2tree.py
+++ file2tree.py
@@ -1,9 +1,9 @@
-# -*-coding:utf-8-*-
+# -*- coding: utf-8 -*-
 import os
 import os.path
 import sys
 import getopt
-
+import platform
 
 def usage():
     print '[ * ] use like "python file2tree.py -r d:/tree"'
@@ -11,64 +11,77 @@ def usage():
 
 
 def get_tree(cur_path):
-    stack = []
-    ret = []
-    stack.append(cur_path)
-    while len(stack) > 0:
-        tmp = stack.pop(len(stack) - 1)
-        if os.path.isdir(tmp):
-            ret.append(tmp)
-            for item in os.listdir(tmp):
-                stack.append(os.path.join(tmp, item))
-            # print tmp
-        elif os.path.isfile(tmp):
-            ret.append(tmp)
-            # print tmp
-    return ret
+	stack=[]
+	ret=[]
+	stack.append(cur_path)
+	while len(stack)>0:
+		tmp=stack.pop(len(stack)-1)
+		if os.path.isdir(tmp):
+			tmp=os.path.normcase(tmp)
+			ret.append(tmp.split(rootdir)[1])
+			for item in os.listdir(tmp):
+				stack.append(os.path.join(tmp,item))
+			#print tmp
+		elif os.path.isfile(tmp):
+			tmp=os.path.normcase(tmp)
+			ret.append(tmp.split(rootdir)[1])
+			#print tmp
+	return ret
 
-
+def get_deepth(path):
+	plat_sys=platform.system()
+	deepth=0
+	if plat_sys == "Windows":
+		path=path.strip("\\")
+		deepth=path.count("\\")+1
+	elif plat_sys == "Linux":
+		path=path.strip("/")
+		deepth=path.count("/")+1
+	return deepth
+	
 def write_tree(rootdir):
-    file_tree = open(os.path.join(rootdir, "/file2tree.txt"), "w")
-    path_list = get_tree(rootdir)
-    length = len(path_list)
-    # file_tree.write("├─"+os.path.split(rootdir)[1]+"\n")
-    for index in range(1, length):
-        tmp = os.path.normcase(path_list[index].split(rootdir)[1])
-        tmp = tmp.strip("\\")
-        deepth = tmp.count("\\") + 1
-        path_name = os.path.split(tmp)[1]
-        if(index < length - 1):
-            tmp_next = os.path.normcase(path_list[index + 1].split(rootdir)[1])
-            tmp_next = tmp_next.strip("\\")
-            deepth_next = tmp_next.count("\\") + 1
-        else:
-            deepth_next = -1
-        if deepth_next == deepth or deepth_next > deepth:
-            space = ""
-            for sp in range(0, deepth - 1):
-                space += "|  "
-            file_tree.write(space + "├─" + path_name + "\n")
-
-        elif deepth_next != deepth:
-            space = ""
-            for sp in range(0, deepth - 1):
-                space += "|  "
-            file_tree.write(space + "└─" + path_name + "\n")
-        # print os.path.split(i)[1]
-    file_tree.close()
-
-
+	file_tree=open(os.path.join(rootdir,"file2tree.txt"),"w")
+	path_list=get_tree(rootdir)
+	length=len(path_list)
+	#file_tree.write("├─"+os.path.split(rootdir)[1]+"\n")
+	for index in range(1,length):
+		tmp=path_list[index]
+		deepth=get_deepth(tmp)
+		if(index<length-1):
+			tmp_next=path_list[index+1]
+			deepth_next=get_deepth(tmp_next)
+		else:
+			deepth_next=-1#│
+			
+		path_name=os.path.split(tmp)[1]
+		
+		if deepth_next==deepth or deepth_next>deepth:
+			space=""
+			for sp in range(0,deepth-1):
+				space+="│  "
+			file_tree.write(space+"├─"+path_name+"\n")
+	
+		elif deepth_next<deepth :
+			space=""
+			for sp in range(0,deepth-1):
+				space+="│  "
+			file_tree.write(space+"└─"+path_name+"\n")
+		#print os.path.split(i)[1]
+	file_tree.close()
+	
+	
 opts, args = getopt.getopt(sys.argv[1:], "hr:", ["help", "rootdir="])
 if not opts:
-    usage()
+	usage()
 for op, value in opts:
-    if op == "-r" or op == "--rootdir":
-        rootdir = value
-        if not os.path.exists(rootdir):
-            print '[ * ] path "%s" does not exist,please check the param "-r"' % (rootdir)
-            sys.exit()
-        else:
-            write_tree(rootdir.rstrip("\\"))
-    else:
-        usage()
-        sys.exit()
+	if op == "-r" or op == "--rootdir":
+		rootdir = value
+		if not os.path.exists(rootdir):
+			print '[ * ] path "%s" does not exist,please check the param "-r"' % (rootdir)
+			sys.exit()
+		else:
+			rootdir=os.path.normcase(rootdir)
+			write_tree(rootdir.rstrip("\\"))	
+	else:
+		usage()
+		sys.exit()
